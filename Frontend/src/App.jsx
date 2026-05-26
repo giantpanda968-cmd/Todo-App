6159365388c7dac9a9ec2cbd1aebb70c74be1e7e
Abhishek <giantpanda968@gmail.com>
2026-05-26 15:19:24 +0530

    Resposive

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -19,63 +19,64 @@ const App = () => {
  }
 
   return (
-    <div className='w-full min-h-screen bg-blue-600 flex justify-center items-start gap-5'>
+    <div className='w-full min-h-screen bg-blue-600 flex justify-center items-start gap-5 p-5'>
 
       {/* your Task Page */}
     
-      <div className='w-75 h-fit bg-gray-200 rounded-xl p-4 mt-5'>
+      <div className='w-full h-fit bg-gray-200 rounded-xl p-4'>
       
 
         {/* Add Task Section */}
 
         
 
-          <form className='w-full h-fit flex gap-2' onSubmit={(e)=>{
+          <form className='w-full h-fit flex flex-col gap-5' onSubmit={(e)=>{
             onSubmithandler(e)
           }}>
-          <input type="text" placeholder='Add new task...' className='w-full outline-0 border border-blue-300 py-2  px-3  rounded-md bg-white text-sm font-medium' value={title} onChange={(e)=>{
+          <input type="text" placeholder='Add new task...' className='w-full outline-0 border border-blue-300 py-7  px-5  rounded-md bg-white text-2xl font-medium' value={title} onChange={(e)=>{
             setTitle(e.target.value);
           }}/>
 
-          <button className='px-4 text-white rounded-md bg-blue-500 text-sm cursor-pointer active:opacity-80' onClick={(e)=>{
-            addtask();
-          }}>Add</button>
+          <button className='py-3 text-white rounded-md bg-blue-500 text-xl cursor-pointer active:opacity-80' type='submit'>Add</button>
           </form>
           
 
           {/* Your Activity */}
 
           <div className='w-full h-fit flex justify-evenly items-center mt-5'>
-            <button className='bg-blue-500 py-1 px-3 text-white text-sm text-center rounded-md cursor-pointer active:opacity-80'>All</button>
-            <button className='bg-white py-1 px-3 text-black text-sm text-center rounded-md active:opacity-80 cursor-pointer'>Pending</button>
-            <button className='bg-white py-1 px-3 text-black text-sm text-center rounded-md active:opacity-80 cursor-pointer'>Completed</button>
+
+            <button className='bg-blue-500 py-2 px-4 text-white text-xl text-center rounded-md cursor-pointer active:opacity-80'>All</button>
+
+            <button className='bg-white py-2 px-4 text-black text-xl text-center rounded-md active:opacity-80 cursor-pointer'>Pending</button>
+
+            <button className='bg-white py-2 px-4 text-black text-xl text-center rounded-md active:opacity-80 cursor-pointer'>Completed</button>
           </div>
           <div className='w-full flex justify-center items-center mt-5'>
-            <button className='bg-red-500 text-white py-1 text-sm px-4 rounded-md cursor-pointer active:opacity-80'>Clear All Tasks</button>
+            <button className='bg-red-500 text-white py-2 text-xl px-4 rounded-md cursor-pointer active:opacity-80'>Clear All Tasks</button>
           </div>
 
           {/* Percentage show */}
 
-          <p className='mt-5 text-[11px] whitespace-nowrap'>You have 3 task(s) and you completed 33% out of 100%</p>
+          <p className='mt-5 text-sm whitespace-nowrap'>You have 3 task(s) and you completed 33% out of 100%</p>
          
          {/* Your Task List */}
 
-         <div className='flex flex-col mt-5 gap-2'>
+         <div className='flex flex-col mt-5 gap-3'>
           {task.map((elem,idx)=>{
-            return <div className='w-full bg-white py-2 px-3 rounded-md flex justify-between items-center' key={idx}>
+            return <div className='w-full bg-white py-5 px-3 rounded-md flex justify-between items-center' key={idx}>
                 <div className='flex gap-3'>
-                  <input type="checkbox" className=' border-blue-500'/>
-                  <p className='text-[12px]'>{elem.title}</p>
+                  <input type="checkbox" className='text-xl'/>
+                  <p className='text-xl'>{elem.title}</p>
                 </div>
-                <span className='text-sm  bg-blue-100 px-2  text-center rounded-full cursor-pointer active:opacity-80'>⋮</span>
+                <span className='text-xl  bg-blue-100 px-3  text-center rounded-full cursor-pointer active:opacity-80'>⋮</span>
             </div>
           })}
 
           {/* percentage Bar */}
 
-         <div className='flex justify-center items-center w-full'>
-          <div className='w-45 h-3 bg-gray-300 rounded-2xl overflow-hidden'>
-            <div className='w-15 h-full bg-green-400 rounded-2xl'></div>
+         <div className='flex justify-center items-center w-full mt-5'>
+          <div className='w-75 h-4 bg-gray-300 rounded-2xl overflow-hidden'>
+            <div className='w-35 h-full bg-green-400 rounded-2xl'></div>
           </div>
           </div>
          </div>
